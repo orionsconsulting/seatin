230f712c4544d1628e1ada3a56db27f496a3fb9b
thomas williams <thomas.williams@ark.gov>
2020-08-31 07:08:49 -0500

    added social sidebar added images

---
+++ resources/views/includes/social.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <ul>
+        <li>Subscribe and check out the channel for more top quality Marvel Contest of Champions content and guides!</li>
+
+        <li>
+            If Superchat doesn't work, alternative PayPal donation link for support! -
+            https://www.paypal.com/cgi-bin/webscr...K
+        </li>
+
+        <li>
+            <a src="https://discord.gg/GVV3wMp"><img src="{{'images/discord.png'}}"></a>
+        </li>
+
+        <li>T4B Streak Guide Link: http://i.imgur.com/gkp9fLV.png-</li>
+
+        <li>Follow me on Instagram: https://www.instagram.com/seatinmol (@Seatinmol)</li>
+        <li>Follow me on Twitter: https://www.twitter.com/seatinmol (@Seatinmol)</li>
+        <li>Snapchat: Cjwilliz</li>
+    </ul>
+</div>
